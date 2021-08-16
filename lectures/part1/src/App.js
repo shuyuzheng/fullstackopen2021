05d67f872d3a0a9293ae7e456fdc537089d31b60
Shuyu <shuyu.zheng@helsinki.fi>
2021-08-16 14:43:37 +0300

    commit samples in lecture

---
+++ lectures/part1/src/App.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  const Display = ({ counter }) => <div>{counter}</div>
+  const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+
+  return(
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='plus' />
+      <Button onClick={setToZero} text='zero' />
+      <Button onClick={decreaseByOne} text='minus' />
+    </div>
+  )
+}
+
+export default App
