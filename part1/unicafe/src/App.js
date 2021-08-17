38f0fffa8b2cd884915dc9ee7b77bce5bd280118
Shuyu <shuyu.zheng@helsinki.fi>
2021-08-17 11:33:10 +0300

    Modify the App.js move the definition of react component outside App component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 
+const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>
   const handleGood = () => setGood(good + 1)
   const handleNeutral = () => setNeutral(neutral + 1)
   const handleBad = () => setBad(bad + 1)
