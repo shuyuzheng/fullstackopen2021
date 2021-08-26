431c34549302e87fa2bc929cc2926210166f28d7
Shuyu <shuyu.zheng@helsinki.fi>
2021-08-26 17:10:48 +0300

    finish exercise 2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,44 +1,5 @@
 import React from 'react'
-
-const Header = ({ course }) => {
-  return(
-    <div>
-      <h1>{course.name}</h1>
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((total, part) => total + part.exercises, 0)
-  return (
-    <p><b>Total of {sum} exercises</b></p>
-  )
-}
-
-const Part = ({ part }) => {
-  return (
-    <p key={part.id}>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => <Part part={part} />)}
-    </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
     const courses = [
